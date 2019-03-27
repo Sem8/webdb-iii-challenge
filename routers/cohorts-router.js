cd9607787eb804dacf38d44bea3959991ca2bc4c
Sem8 <mssemmi8@gmail.com>
2019-03-27 17:23:13 -0400

    post request made using knex schema

--- routers/cohorts-router.js
+++ routers/cohorts-router.js
@@ -22,5 +22,18 @@ router.get('/', (req, res) => {
     // res.send('get all cohort data here');
 });
 
+// router.get("/:id")
+
+router.post('/', (req, res) => {
+    cohortsdb('cohorts').insert(req.body).then(ids => {
+        const id = ids[0];
+        cohortsdb('cohorts').where({ id }).first().then(cohort => {
+            res.status(201).json(cohort);
+        });
+    }).catch(error => {
+        res.status(500).json({ message: `A new cohort couldn't be made: ${error}` });
+    });
+});
+
 
 module.exports = router;
