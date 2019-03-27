c1675fcfcfd75dc33eba2a1cb2ab94869ee8c90a
Sem8 <mssemmi8@gmail.com>
2019-03-27 17:09:24 -0400

    table schema created for cohorts using knex

--- routers/cohorts-router.js
+++ routers/cohorts-router.js
@@ -12,14 +12,14 @@ const knexConfig = {
 const cohortsdb = knex(knexConfig);
 
 router.get('/', (req, res) => {
-    // cohortsdb('cohorts')
-    // .then(cohorts => {
-    //     res.status(200).json(cohorts);
-    // })
-    // .catch(error => {
-    //     res.status(500).json({ message: `The cohorts information could not be retrieved: ${error}`}):
-    // });
-    res.send('get all cohort data here');
+    cohortsdb('cohorts')
+    .then(cohorts => {
+        res.status(200).json(cohorts);
+    })
+    .catch(error => {
+        res.status(500).json({ message: `The cohorts information could not be retrieved: ${error}`});
+    });
+    // res.send('get all cohort data here');
 });
 
 
